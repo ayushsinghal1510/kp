0aee99dcc5920fe868d1af060c9336c99d5625f1
Ayush SInghal <ayushsinghal1510@gmail.com>
2026-04-09 17:42:03 +0000

    Fxied

--- retail/rpoc/src/rpoc/app.py
+++ retail/rpoc/src/rpoc/app.py
@@ -6,8 +6,11 @@ import sys
 import re
 import os
 
+from dotenv import load_dotenv
+
 client = Groq(api_key = os.environ['GROQ_API_KEY'])
 
+load_dotenv()
 st.set_page_config(layout = "wide")
 
 CSV_PATH = 'assets/data.csv'
